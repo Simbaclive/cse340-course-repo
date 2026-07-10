d23f3fac730671e978a6e2b2c2a519da2f027ea0
Clive <simbaclive@gmail.com>
2026-07-10 16:12:45 +0200

    Fix home route rendering and update error handling

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home Page' });
+    res.render('home', { title: 'Home Page' });
 });
 
 app.get('/organizations', async (req, res) => {
